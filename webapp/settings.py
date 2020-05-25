0b90c686a80f159ee3981ab2ce573f46a0458ab7
Ankit Maheshwari <infoankit130@gmail.com>
2020-05-25 15:29:30 +0530

    asdas

--- webapp/settings.py
+++ webapp/settings.py
@@ -129,4 +129,4 @@ import dj_database_url
 prod_db  =  dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
 
-ALLOWED_HOSTS = ['ankit-neural.herokuapp.com']
+ALLOWED_HOSTS = ['shrouded-reaches-80883.herokuapp.com']
